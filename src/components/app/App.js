3fac03ee46b44b459e6b434661c93d909f8282ce
mr-t-Durden <67805487+mr-t-Durden@users.noreply.github.com>
2021-12-13 15:07:53 +0100

    Prepare for save playlist feature

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -54,6 +54,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track) {
@@ -77,6 +78,10 @@ class App extends React.Component {
     });
   }
 
+  savePlaylist() {
+    const trackURIs = playlistTracks.map((track) => track.uri);
+  }
+
   render() {
     return (
       <div>
@@ -93,6 +98,7 @@ class App extends React.Component {
               playlistTracks={this.state.playlistTracks} 
               onRemove={this.removeTrack}
               onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist}
             />
           </div>
         </div>
